a4840b332c7ff54b8ed50a8d5c74fda8886ad281
codingclub <codingclub@nitm.ac.in>
2023-12-16 08:50:24 +0530

    Authorize and Protect Routes

--- routes/event.js
+++ routes/event.js
@@ -2,9 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-const EventController = require('../controller/event')
+const EventController = require('../controller/event');
+const { protect, authorize } = require('../middleware/auth');
 
-router.route('/').post(EventController.createEvent).get(EventController.getAllEvents);
-router.route('/:eventId').put(EventController.updateEvent).delete(EventController.deleteEvent).get(EventController.getSingleEvent)
+router.route('/').post(protect, authorize('ADMIN', 'TEAM_MEMBER'), EventController.createEvent).get(EventController.getAllEvents);
+router.route('/:eventId').put(protect, authorize('ADMIN', 'TEAM_MEMBER'), EventController.updateEvent).delete(protect, authorize('ADMIN', 'TEAM_MEMBER'), EventController.deleteEvent).get(EventController.getSingleEvent)
 
 module.exports = router;
