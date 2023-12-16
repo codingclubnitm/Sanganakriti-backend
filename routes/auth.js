3f2b7d6fecd833c9a526eebca531653e8c421df1
codingclub <codingclub@nitm.ac.in>
2023-12-16 09:01:00 +0530

    Logout Route Created

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { createUser, loginUser } = require('../controller/auth');
+const { createUser, loginUser, logoutUser } = require('../controller/auth');
+const { protect } = require('../middleware/auth');
 const singleUpload = require('../middleware/multer');
 
 const router = express.Router();
 
 router.route('/register').post(singleUpload, createUser);
 router.route('/login').post(loginUser)
+router.route('/logout').get(protect, logoutUser);
 
 module.exports = router;
